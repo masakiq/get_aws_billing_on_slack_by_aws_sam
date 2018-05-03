2e80f8c1d3f2e8b4bab8bb8f0eda30a96241b4fc
maeda1150 <masaki19770108@gmail.com>
2018-05-03 18:56:52 +0900

    move GetBilling

---
+++ awsapi/get_billing.go
@@ -0,0 +1,77 @@
+package awsapi
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudwatch"
+)
+
+const (
+	region         = "us-east-1"
+	namespace      = "AWS/Billing"
+	metricName     = "EstimatedCharges"
+	dimensionName  = "Currency"
+	dimensionValue = "USD"
+	period6hours   = 21600
+	periodDay      = 86400
+)
+
+func GetBilling() (float64, error) {
+	sess, err := session.NewSession(&aws.Config{Region: aws.String(region)})
+	if err != nil {
+		log.Println(err)
+		return 0, err
+	}
+
+	svc := cloudwatch.New(sess)
+
+	params := &cloudwatch.GetMetricStatisticsInput{
+		Namespace:  aws.String(namespace),
+		MetricName: aws.String(metricName),
+		Period:     aws.Int64(period6hours),
+		StartTime:  aws.Time(time.Now().Add(time.Duration(periodDay) * time.Second * -1)),
+		EndTime:    aws.Time(time.Now()),
+		Statistics: []*string{
+			aws.String(cloudwatch.StatisticMaximum),
+		},
+		Dimensions: []*cloudwatch.Dimension{
+			{
+				Name:  aws.String(dimensionName),
+				Value: aws.String(dimensionValue),
+			},
+		},
+		Unit: aws.String(cloudwatch.StandardUnitNone),
+	}
+
+	resp, err := svc.GetMetricStatistics(params)
+	if err != nil {
+		log.Println(err)
+		return 0, err
+	}
+
+	log.Println(*resp)
+	if len(resp.Datapoints) < 1 {
+		return 0, fmt.Errorf("Datapoint is 0. Sould extends get Datapoint start time.")
+	}
+
+	bills := []float64{}
+	for _, bill := range resp.Datapoints {
+		bills = append(bills, float64(*bill.Maximum))
+	}
+
+	return max(bills), nil
+}
+
+func max(a []float64) float64 {
+	max := a[0]
+	for _, i := range a {
+		if i > max {
+			max = i
+		}
+	}
+	return max
+}
--- main.go
+++ main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"get_aws_billing_on_slack_by_aws_sam/awsapi"
 	"log"
 	"strconv"
-	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/cloudwatch"
 	"github.com/aws/aws-sdk-go/service/ssm"
 	"github.com/awslabs/aws-lambda-go-api-proxy/gin"
 	"github.com/gin-gonic/gin"
@@ -45,68 +43,12 @@ func getAwsCosts(c *gin.Context) {
 		c.String(400, "Incorrect token")
 		return
 	}
-	billing, err := GetBilling()
+	billing, err := awsapi.GetBilling()
 	if err != nil {
 		c.String(400, "Get Billing error")
 		return
 	}
 	c.String(200, strconv.FormatFloat(billing, 'f', 4, 64)+" $")
-}
-
-const (
-	region         = "us-east-1"
-	namespace      = "AWS/Billing"
-	metricName     = "EstimatedCharges"
-	dimensionName  = "Currency"
-	dimensionValue = "USD"
-	period6hours   = 21600
-	periodDay      = 86400
-)
-
-func GetBilling() (float64, error) {
-	sess, err := session.NewSession(&aws.Config{Region: aws.String(region)})
-	if err != nil {
-		log.Println(err)
-		return 0, err
-	}
-
-	svc := cloudwatch.New(sess)
-
-	params := &cloudwatch.GetMetricStatisticsInput{
-		Namespace:  aws.String(namespace),
-		MetricName: aws.String(metricName),
-		Period:     aws.Int64(period6hours),
-		StartTime:  aws.Time(time.Now().Add(time.Duration(periodDay) * time.Second * -1)),
-		EndTime:    aws.Time(time.Now()),
-		Statistics: []*string{
-			aws.String(cloudwatch.StatisticMaximum),
-		},
-		Dimensions: []*cloudwatch.Dimension{
-			{
-				Name:  aws.String(dimensionName),
-				Value: aws.String(dimensionValue),
-			},
-		},
-		Unit: aws.String(cloudwatch.StandardUnitNone),
-	}
-
-	resp, err := svc.GetMetricStatistics(params)
-	if err != nil {
-		log.Println(err)
-		return 0, err
-	}
-
-	log.Println(*resp)
-	if len(resp.Datapoints) < 1 {
-		return 0, fmt.Errorf("Datapoint is 0. Sould extends get Datapoint start time.")
-	}
-
-	bills := []float64{}
-	for _, bill := range resp.Datapoints {
-		bills = append(bills, float64(*bill.Maximum))
-	}
-
-	return max(bills), nil
 }
 
 func SsmGet(name string, region string) (string, error) {
@@ -126,14 +68,4 @@ func SsmGet(name string, region string) (string, error) {
 		return "", err
 	}
 	return *param.Parameter.Value, nil
-}
-
-func max(a []float64) float64 {
-	max := a[0]
-	for _, i := range a {
-		if i > max {
-			max = i
-		}
-	}
-	return max
 }
